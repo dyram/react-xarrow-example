08456cd330b55364737e3cac49f672bbe44addcd
Dyram <dyram@Dyrams-Air.Dlink>
2022-01-05 18:37:02 +0530

    Panel group causes breaking changes in line drawing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import './styles.css';
 import { useState } from 'react';
 import React from 'react';
+import PanelGroup from 'react-panelgroup';
 
-// components
+// custom-components
 import Line from './components/Line';
 
 export default function App() {
@@ -13,7 +14,7 @@ export default function App() {
   const [selectedConnections, setSelectedConnections] = useState([]);
 
   //tests
-  // const [variableHeight, setVariableHeight] = useState('200vh');
+  const [variableHeight, setVariableHeight] = useState('200vh');
 
   const clearSelection = () => {
     setSelectedLeftNode(undefined);
@@ -107,64 +108,6 @@ export default function App() {
         </button>
       </div>
 
-      <div className="operation-container">
-        <div className="left">
-          <div
-            className={`node ${
-              selectedLeftNode === 'item_left' ? `selected` : ``
-            }`}
-            id="item_left"
-            onClick={() => clickNode('left', 'item_left')}
-          >
-            Left Node
-          </div>
-          {/* <XArrow start={"item_left"} end={"item_right"} /> */}
-          <div
-            className={`node ${
-              selectedLeftNode === 'item_left2' ? `selected` : ``
-            }`}
-            id="item_left2"
-            onClick={() => clickNode('left', 'item_left2')}
-          >
-            Left Node 2
-          </div>
-          <div
-            className={`node ${
-              selectedLeftNode === 'item_left3' ? `selected` : ``
-            }`}
-            id="item_left3"
-            onClick={() => clickNode('left', 'item_left3')}
-          >
-            Left Node 3
-          </div>
-        </div>
-        <div className="right">
-          <div
-            className={`node ${
-              selectedRightNode === 'item_right' ? `selected` : ``
-            }`}
-            id="item_right"
-            onClick={() => clickNode('right', 'item_right')}
-          >
-            Right Node
-          </div>
-          {/* <div
-            style={{ height: variableHeight }}
-            onClick={() => setVariableHeight('20vh')}
-          ></div> */}
-          <div
-            className={`node ${
-              selectedRightNode === 'item_right2' ? `selected` : ``
-            }`}
-            id="item_right2"
-            onClick={() => clickNode('right', 'item_right2')}
-          >
-            Right Node 2
-          </div>
-          {/* <button onClick={remove}>Remove</button> */}
-        </div>
-      </div>
-
       <div id="xarrow-container">
         {connections.map((connection) => (
           <span
@@ -182,6 +125,87 @@ export default function App() {
           </span>
         ))}
       </div>
+
+      <PanelGroup
+        borderColor="gray"
+        direction="column"
+        panelWidths={[
+          { minSize: 100, resize: 'stretch' },
+          { minSize: 100, resize: 'stretch' },
+        ]}
+      >
+        <PanelGroup
+          borderColor="red"
+          panelWidths={[
+            { minSize: 100, resize: 'stretch' },
+            { minSize: 100, resize: 'stretch' },
+            { minSize: 100, resize: 'stretch' },
+          ]}
+          direction="row"
+        >
+          {/* SOURCE PANE */}
+          <div className="pane">
+            <div
+              className={`node ${
+                selectedLeftNode === 'item_left' ? `selected` : ``
+              }`}
+              id="item_left"
+              onClick={() => clickNode('left', 'item_left')}
+            >
+              Left Node
+            </div>
+            <div
+              className={`node ${
+                selectedLeftNode === 'item_left2' ? `selected` : ``
+              }`}
+              id="item_left2"
+              onClick={() => clickNode('left', 'item_left2')}
+            >
+              Left Node 2
+            </div>
+            <div
+              className={`node ${
+                selectedLeftNode === 'item_left3' ? `selected` : ``
+              }`}
+              id="item_left3"
+              onClick={() => clickNode('left', 'item_left3')}
+            >
+              Left Node 3
+            </div>
+          </div>
+          {/* TARGET PANE */}
+          <div className="pane">
+            <div
+              className={`node ${
+                selectedRightNode === 'item_right' ? `selected` : ``
+              }`}
+              id="item_right"
+              onClick={() => clickNode('right', 'item_right')}
+            >
+              Right Node
+            </div>
+            <div
+              style={{ height: variableHeight }}
+              onClick={() => setVariableHeight('20vh')}
+            ></div>
+            <div
+              className={`node ${
+                selectedRightNode === 'item_right2' ? `selected` : ``
+              }`}
+              id="item_right2"
+              onClick={() => clickNode('right', 'item_right2')}
+            >
+              Right Node 2
+            </div>
+          </div>
+          <div className="pane">
+            <div>Functions</div>
+          </div>
+        </PanelGroup>
+        <div className="pane">
+          <h1>Error popup from bottom</h1>
+        </div>
+      </PanelGroup>
     </>
   );
 }
