aca8b82023f56d556781232141ea67c4974f8e53
Dyram <dyram@Dyrams-Air.Dlink>
2022-01-05 19:24:04 +0530

    Scroll line update problem

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import './styles.css';
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import React from 'react';
+import { useXarrow, Xwrapper } from 'react-xarrows';
 
 // components
 import Line from './components/Line';
 
 export default function App() {
+  const updateXarrow = useXarrow();
+
+  //refs
+  const rightPane = useRef(null);
+
+  useEffect(() => {
+    // rightPane.current.onscroll = handleScroll;
+  }, []);
+
+  const handleScroll = () => {
+    console.log('Scrolling');
+  };
+
   // state
   const [selectedLeftNode, setSelectedLeftNode] = useState();
   const [selectedRightNode, setSelectedRightNode] = useState();
@@ -13,7 +27,7 @@ export default function App() {
   const [selectedConnections, setSelectedConnections] = useState([]);
 
   //tests
-  // const [variableHeight, setVariableHeight] = useState('200vh');
+  const [variableHeight, setVariableHeight] = useState('200vh');
 
   const clearSelection = () => {
     setSelectedLeftNode(undefined);
@@ -82,106 +96,108 @@ export default function App() {
   };
 
   return (
-    <>
-      <div className="header">
-        <button
-          onClick={removeSelectedConnections}
-          disabled={selectedConnections.length < 1}
-          className={
-            selectedConnections.length < 1 ? `disabled-btn` : `btn danger-btn`
-          }
-        >
-          Remove Selected Connections
-        </button>
-
-        <button
-          onClick={removeAllConnections}
-          disabled={connections.length < 1}
-          className={
-            connections.length < 1
-              ? `disabled-btn ml-10`
-              : `btn danger-btn ml-10`
-          }
-        >
-          Remove All Connections
-        </button>
-      </div>
-
-      <div className="operation-container">
-        <div className="left">
-          <div
-            className={`node ${
-              selectedLeftNode === 'item_left' ? `selected` : ``
-            }`}
-            id="item_left"
-            onClick={() => clickNode('left', 'item_left')}
-          >
-            Left Node
-          </div>
-          {/* <XArrow start={"item_left"} end={"item_right"} /> */}
-          <div
-            className={`node ${
-              selectedLeftNode === 'item_left2' ? `selected` : ``
-            }`}
-            id="item_left2"
-            onClick={() => clickNode('left', 'item_left2')}
-          >
-            Left Node 2
-          </div>
-          <div
-            className={`node ${
-              selectedLeftNode === 'item_left3' ? `selected` : ``
-            }`}
-            id="item_left3"
-            onClick={() => clickNode('left', 'item_left3')}
-          >
-            Left Node 3
-          </div>
-        </div>
-        <div className="right">
-          <div
-            className={`node ${
-              selectedRightNode === 'item_right' ? `selected` : ``
-            }`}
-            id="item_right"
-            onClick={() => clickNode('right', 'item_right')}
-          >
-            Right Node
-          </div>
-          {/* <div
-            style={{ height: variableHeight }}
-            onClick={() => setVariableHeight('20vh')}
-          ></div> */}
-          <div
-            className={`node ${
-              selectedRightNode === 'item_right2' ? `selected` : ``
-            }`}
-            id="item_right2"
-            onClick={() => clickNode('right', 'item_right2')}
-          >
-            Right Node 2
-          </div>
-          {/* <button onClick={remove}>Remove</button> */}
-        </div>
-      </div>
-
-      <div id="xarrow-container">
-        {connections.map((connection) => (
-          <span
-            onClick={() =>
-              clickLine(`${connection.source}-${connection.target}`)
+    <div>
+      <Xwrapper>
+        <div className="header">
+          <button
+            onClick={removeSelectedConnections}
+            disabled={selectedConnections.length < 1}
+            className={
+              selectedConnections.length < 1 ? `disabled-btn` : `btn danger-btn`
             }
-            key={`${connection.source}-${connection.target}`}
-            className="pointed xarrow-span"
           >
-            <Line
-              id={`${connection.source}-${connection.target}`}
-              connection={connection}
-              selectedConnections={selectedConnections}
-            />
-          </span>
-        ))}
-      </div>
-    </>
+            Remove Selected Connections
+          </button>
+
+          <button
+            onClick={removeAllConnections}
+            disabled={connections.length < 1}
+            className={
+              connections.length < 1
+                ? `disabled-btn ml-10`
+                : `btn danger-btn ml-10`
+            }
+          >
+            Remove All Connections
+          </button>
+        </div>
+
+        <div className="operation-container">
+          <div className="left">
+            <div
+              className={`node ${
+                selectedLeftNode === 'item_left' ? `selected` : ``
+              }`}
+              id="item_left"
+              onClick={() => clickNode('left', 'item_left')}
+            >
+              Left Node
+            </div>
+            {/* <XArrow start={"item_left"} end={"item_right"} /> */}
+            <div
+              className={`node ${
+                selectedLeftNode === 'item_left2' ? `selected` : ``
+              }`}
+              id="item_left2"
+              onClick={() => clickNode('left', 'item_left2')}
+            >
+              Left Node 2
+            </div>
+            <div
+              className={`node ${
+                selectedLeftNode === 'item_left3' ? `selected` : ``
+              }`}
+              id="item_left3"
+              onClick={() => clickNode('left', 'item_left3')}
+            >
+              Left Node 3
+            </div>
+          </div>
+          <div className="right" ref={rightPane}>
+            <div
+              className={`node ${
+                selectedRightNode === 'item_right' ? `selected` : ``
+              }`}
+              id="item_right"
+              onClick={() => clickNode('right', 'item_right')}
+            >
+              Right Node
+            </div>
+            <div
+              style={{ height: variableHeight }}
+              onClick={() => setVariableHeight('20vh')}
+            ></div>
+            <div
+              className={`node ${
+                selectedRightNode === 'item_right2' ? `selected` : ``
+              }`}
+              id="item_right2"
+              onClick={() => clickNode('right', 'item_right2')}
+            >
+              Right Node 2
+            </div>
+            {/* <button onClick={remove}>Remove</button> */}
+          </div>
+        </div>
+
+        <div id="xarrow-container">
+          {connections.map((connection) => (
+            <span
+              onClick={() =>
+                clickLine(`${connection.source}-${connection.target}`)
+              }
+              key={`${connection.source}-${connection.target}`}
+              className="pointed xarrow-span"
+            >
+              <Line
+                id={`${connection.source}-${connection.target}`}
+                connection={connection}
+                selectedConnections={selectedConnections}
+              />
+            </span>
+          ))}
+        </div>
+      </Xwrapper>
+    </div>
   );
 }
